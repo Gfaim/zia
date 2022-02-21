0cba43a3a62d9ba567b9e79b7de7c68b42682166
Martin Olivier <58467105+martin-olivier@users.noreply.github.com>
2022-02-21 18:20:50 +0100

    feat: draft main socket listen

--- modules/src/http/Module.cpp
+++ modules/src/http/Module.cpp
@@ -1,9 +1,11 @@
 #include "http/Module.hpp"
 
+#include <asio.hpp>
 #include <iostream>
 #include <thread>
 
 #include "dylib/dylib.hpp"
+#include "ziapi/Logger.hpp"
 
 const char *HttpModule::kModuleName = "HTTP";
 
@@ -11,44 +13,29 @@ const char *HttpModule::kModuleDescription = "TCP network layer and HTTP parser"
 
 void HttpModule::Run(ziapi::http::IRequestOutputQueue &requests, ziapi::http::IResponseInputQueue &responses)
 {
-    auto now_plus_some_time = std::chrono::system_clock::now() + std::chrono::seconds(3);
+    asio::ip::tcp::acceptor acceptor(ctx_);
 
-    while (true) {
-        if (now_plus_some_time < std::chrono::system_clock::now()) {
-            ziapi::http::Request req{};
-            ziapi::http::Context ctx{};
-            requests.Push(std::make_pair(req, ctx));
-            std::cout << "BIM une requête" << std::endl;
-            now_plus_some_time = std::chrono::system_clock::now() + std::chrono::seconds(1);
-        }
-        if (responses.Size()) {
-            auto res_opt = responses.Pop();
-            if (res_opt) {
-                auto res = res_opt->first;
-                std::cout << "<Response code=" << (int)res.status_code << ", body_size=" << res.body.size() << ">"
-                          << std::endl;
-            }
-        }
-        if (must_stop_) {
-            /// We set must_stop_ to false to counteract spurious wakeups inside Terminate().
-            must_stop_ = false;
-            has_stopped_.notify_all();
-            break;
-        }
-        std::this_thread::sleep_for(std::chrono::nanoseconds(1000));
-    }
+    AsyncAccept();
+    StartThreadPool();
 }
 
-void HttpModule::Terminate()
+void HttpModule::AsyncAccept()
 {
-    std::mutex mu;
-    std::unique_lock<std::mutex> ul{mu};
-    must_stop_ = true;
-    /// Once the worker thread has gracefully shut down, it will set must_stop_ back to false.
-    /// We wait on the condition variable in a loop to counteract spurious wakeups.
-    while (must_stop_) {
-        has_stopped_.wait(ul);
+    acceptor_.async_accept([&](const auto &ec, auto socket) {
+        if (ec) {
+        }
+        ziapi::Logger::Info("New connection established");
+        clients_.emplace_back(std::make_pair(std::move(socket), asio::make_strand(ctx_)));
+    });
+}
+
+void HttpModule::StartThreadPool()
+{
+    for (size_t i = 0; i < num_threads_; i++) {
+        thread_pool_.emplace_back([&]() { ctx_.run(); });
     }
 }
 
-DYLIB_API ziapi::IModule *LoadZiaModule() { return new HttpModule(); }
+void HttpModule::Terminate() {}
+
+DYLIB_API ziapi::IModule *LoadZiaModule() { return new HttpModule(5); }
