7b051793d5bd644f3d00df586296a143f0883f53
Allan <allan.debeve@epitech.eu>
2022-02-22 14:56:25 +0100

    feat: add example modules of the ziapi directory listing, logger

---
+++ modules/include/directory_listing/Module.hpp
@@ -0,0 +1,32 @@
+#include <filesystem>
+#include <fstream>
+
+#include "ziapi/Config.hpp"
+#include "ziapi/Module.hpp"
+
+class DirectoryListingModule : public ziapi::IHandlerModule {
+public:
+    DirectoryListingModule() : root_("") {}
+
+    void Init(const ziapi::config::Node &cfg) override;
+
+    [[nodiscard]] ziapi::Version GetVersion() const noexcept override { return {3, 1, 0}; }
+
+    [[nodiscard]] ziapi::Version GetCompatibleApiVersion() const noexcept override { return {3, 1, 0}; }
+
+    [[nodiscard]] const char *GetName() const noexcept override { return "DirectoryListing"; }
+
+    [[nodiscard]] const char *GetDescription() const noexcept override
+    {
+        return "Give access to a filesystem over HTTP";
+    }
+
+    [[nodiscard]] double GetHandlerPriority() const noexcept override;
+
+    [[nodiscard]] bool ShouldHandle(const ziapi::http::Context &ctx, const ziapi::http::Request &req) const override;
+
+    void Handle(ziapi::http::Context &ctx, const ziapi::http::Request &req, ziapi::http::Response &res) override;
+
+private:
+    std::string root_;
+};
---
+++ modules/src/logger/Module.cpp
@@ -0,0 +1,48 @@
+#include "logger/Module.hpp"
+
+#include "dylib/dylib.hpp"
+
+[[nodiscard]] double LoggerModule::GetPostProcessorPriority() const noexcept { return 1; }
+
+[[nodiscard]] bool LoggerModule::ShouldPostProcess(const ziapi::http::Context &ctx,
+                                                   const ziapi::http::Response &res) const
+{
+    return true;
+}
+
+[[nodiscard]] double LoggerModule::GetPreProcessorPriority() const noexcept { return 0; }
+
+[[nodiscard]] bool LoggerModule::ShouldPreProcess(const ziapi::http::Context &ctx,
+                                                  const ziapi::http::Request &req) const
+{
+    return true;
+}
+
+void LoggerModule::PostProcess(ziapi::http::Context &ctx, ziapi::http::Response &res)
+{
+    std::stringstream ss;
+
+    // Exemple: ` [X] 404: Not found (GET /test, 2s)`
+    ss << std::to_string((int)res.status_code) << ": " << res.reason << " ("
+       << std::any_cast<std::string>(ctx["method"]) << " " << std::any_cast<std::string>(ctx["target"]) << ", "
+       << std::setprecision(2) << difftime(std::time(nullptr), std::any_cast<time_t>(ctx["timestamp"])) << "s)";
+    if (!res.body.empty()) {
+        ss << std::endl << "BODY:\n\"" << res.body << "\"";
+    }
+    if ((int)res.status_code < 300) {
+        ziapi::Logger::Info(ss.str());
+    } else if ((int)res.status_code < 400) {
+        ziapi::Logger::Warning(ss.str());
+    } else {
+        ziapi::Logger::Error(ss.str());
+    }
+}
+
+void LoggerModule::PreProcess(ziapi::http::Context &ctx, ziapi::http::Request &req)
+{
+    ctx["timestamp"] = std::time(nullptr);
+    ctx["target"] = req.target;
+    ctx["method"] = req.method;
+}
+
+DYLIB_API ziapi::IModule *LoadZiaModule() { return new LoggerModule(); }
