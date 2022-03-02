5f8d31660ac77c0146ea1cc4486864e3734f619c
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2022-03-02 20:29:16 +0100

    fix: RequestToString to ResponseToString

--- modules/include/http/RequestToString.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "ziapi/Http.hpp"
-
-std::string RequestToString(const ziapi::http::Request &r);
--- modules/src/http/ResponseToString.cpp
+++ modules/src/http/ResponseToString.cpp
@@ -1,9 +1,9 @@
-#include "http/RequestToString.hpp"
-
 #include <iomanip>
 #include <sstream>
 
-std::string RequestToString(const ziapi::http::Request &r)
+#include "http/ResponseToString.hpp"
+
+std::string ResponseToString(const ziapi::http::Response &r)
 {
     float version = static_cast<float>(r.version) / 10.f;
     std::string version_str;
@@ -12,7 +12,8 @@ std::string RequestToString(const ziapi::http::Request &r)
 
     version_stream << std::fixed << std::setprecision(1) << version;
     version_str = version_stream.str();
-    res = r.method + " " + r.target + " HTTP/" + version_str + "\r\n";
+    res = "HTTP/" + version_str + " " + std::to_string(static_cast<std::size_t>(r.status_code)) + " " +
+          kStatusCodeMap.at(r.status_code) + "\r\n";
     for (const auto &[key, value] : r.headers) {
         res += key + ": " + value + "\r\n";
     }
