b1dcae7baa8fbf01f9c26c6efa3589f2b31944f2
Diego ROJAS <63368264+rojasdiegopro@users.noreply.github.com>
2022-03-02 21:00:19 +0100

    fix: crash in http request

--- modules/src/http/Connection.cpp
+++ modules/src/http/Connection.cpp
@@ -7,48 +7,55 @@
 #include "http/ConnectionManager.hpp"
 
 Connection::Connection(asio::ip::tcp::socket socket, SafeRequestQueue &requests, ConnectionManager &conn_manager)
-    : socket_(std::move(socket)), requests_(requests), conn_manager_(conn_manager), should_close_(true)
+    : socket_(std::move(socket)),
+      requests_(requests),
+      conn_manager_(conn_manager),
+      buffer_(),
+      remote_endpoint_(),
+      outbuf_(),
+      should_close_(true),
+      is_open_(true)
 {
+    try {
+        remote_endpoint_ = socket_.remote_endpoint();
+    } catch (...) {
+    }
 }
 
 void Connection::Start() { DoRead(); }
 
 void Connection::DoRead()
 {
-    socket_.async_read_some(asio::buffer(buffer_), [this, me = shared_from_this()](auto ec, auto bytes_read) {
-        if (ec) {
-            if (socket_.is_open()) {
-                conn_manager_.Close(me);
+    socket_.async_read_some(asio::buffer(buffer_), [this, me = shared_from_this()](auto ec, auto) {
+        CallbackWrapper(ec, [this]() {
+            {
+                /// TODO: Do something with the buffer.
+                ziapi::http::Request req;
+                ziapi::http::Context ctx;
+                req.version = ziapi::http::Version::kV1_1;
+                req.target = "/index.html";
+                req.method = ziapi::http::method::kGet;
+                req.headers.emplace(ziapi::http::header::kAuthorization, "Basic diego:mdp");
+                req.body = "Hello world!";
+                ctx.emplace("client.socket.address",
+                            std::make_any<std::string>(remote_endpoint_.address().to_string()));
+                ctx.emplace("client.socket.port", std::make_any<std::uint16_t>(remote_endpoint_.port()));
+                ctx.emplace("http.connection", std::make_any<std::string>("close"));
+                requests_.Push(std::make_pair(std::move(req), std::move(ctx)));
             }
-            return;
-        }
-        {
-            /// TODO: Do something with the buffer.
-            ziapi::Logger::Debug("incoming message of size ", bytes_read);
-            ziapi::http::Request req;
-            ziapi::http::Context ctx;
-            req.version = ziapi::http::Version::kV1_1;
-            req.target = "/index.html";
-            req.method = ziapi::http::method::kGet;
-            req.headers.emplace(ziapi::http::header::kAuthorization, "Basic diego:mdp");
-            req.body = "Hello world!";
-            ctx.emplace("client.socket.address",
-                        std::make_any<std::string>(socket_.remote_endpoint().address().to_string()));
-            ctx.emplace("client.socket.port", std::make_any<std::uint16_t>(socket_.remote_endpoint().port()));
-            ctx.emplace("http.connection", std::make_any<std::string>("close"));
-            requests_.Push(std::make_pair(std::move(req), std::move(ctx)));
-        }
-        DoRead();
+            DoRead();
+        });
     });
 }
 
 void Connection::DoWrite()
 {
-    asio::async_write(socket_, asio::buffer(outbuf_), [this, me = shared_from_this()](auto ec, auto bytes_written) {
-        if ((ec || should_close_) && socket_.is_open()) {
-            conn_manager_.Close(me);
-            return;
-        }
+    asio::async_write(socket_, asio::buffer(outbuf_), [this, me = shared_from_this()](auto ec, auto) {
+        CallbackWrapper(ec, [this, &me]() {
+            if (IsOpen() && should_close_) {
+                conn_manager_.Close(me);
+            }
+        });
     });
 }
 
@@ -67,6 +74,21 @@ void Connection::Send(const ziapi::http::Response &)
 
 void Connection::ShouldClose(bool should_close) { should_close_ = should_close; }
 
-asio::ip::tcp::endpoint Connection::RemoteEndpoint() const { return socket_.remote_endpoint(); }
+bool Connection::IsOpen()
+{
+    if (is_open_ && socket_.is_open()) {
+        return true;
+    }
+    is_open_ = false;
+    return false;
+}
 
-void Connection::Close() { socket_.close(); }
+const asio::ip::tcp::endpoint &Connection::RemoteEndpoint() const { return remote_endpoint_; }
+
+void Connection::Close()
+{
+    if (is_open_ && socket_.is_open()) {
+        is_open_ = false;
+        socket_.close();
+    }
+}
