db47dfc570bb72779b471d279c11bfa1cb4498a1
Martin Olivier <martin.olivier@live.fr>
2022-03-03 10:28:36 +0100

    fix: CI

--- import/FetchYamlCpp.cmake
+++ import/FetchYamlCpp.cmake
@@ -6,14 +6,16 @@ FetchContent_Declare(
     yamlcpp
     GIT_REPOSITORY  "https://github.com/jbeder/yaml-cpp"
     GIT_TAG         "yaml-cpp-0.7.0"
-    BUILD_COMMAND   ${CMAKE_COMMAND} -DYAML_CPP_BUILD_TESTS=OFF .
-    COMMAND         ${CMAKE_COMMAND} --build .
-    INSTALL_COMMAND ""
-    TEST_COMMAND    ""
 )
 
 FetchContent_MakeAvailable(yamlcpp)
 include_directories(${yamlcpp_SOURCE_DIR}/include)
+
+if(NOT yamlcpp_POPULATED)
+  FetchContent_Populate(yamlcpp)
+  set(YAML_CPP_BUILD_TESTS OFF)
+  add_subdirectory(yamlcpp)
+endif()
 
 if(WIN32)
     link_directories(${yamlcpp_INSTALL_DIR}/src/libyaml-cpp-build/Debug)
