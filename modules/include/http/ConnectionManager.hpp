a170d7a342066485f0b1f8258c5b4b46bb2af5e5
Diego ROJAS <63368264+rojasdiegopro@users.noreply.github.com>
2022-03-04 20:39:50 +0100

    feat: generic connection manager

--- modules/include/http/ConnectionManager.hpp
+++ modules/include/http/ConnectionManager.hpp
@@ -1,6 +1,68 @@
 #pragma once
 
 #include <memory>
-#include <utility>
+#include <mutex>
+#include <ziapi/Logger.hpp>
 
-#include "Connection.hpp"
+template <typename TConnection>
+class ConnectionManager {
+public:
+    using SharedConnection = std::shared_ptr<TConnection>;
+
+    void Add(SharedConnection conn)
+    {
+        std::scoped_lock sl(mu_);
+        connections_.emplace_back(conn);
+        ziapi::Logger::Debug("new connection established ", conn->RemoteEndpoint());
+        conn->Start();
+    }
+
+    void Close(SharedConnection conn)
+    {
+        std::scoped_lock sl(mu_);
+        connections_.erase(std::remove_if(connections_.begin(), connections_.end(),
+                                          [conn](const auto &el) { return (el.get() == conn.get()); }));
+        ziapi::Logger::Debug("client disconnected ", conn->RemoteEndpoint());
+        conn->Close();
+    }
+
+    void CloseAll()
+    {
+        std::scoped_lock sl(mu_);
+        for (auto &conn : connections_) {
+            ziapi::Logger::Debug("client disconnected ", conn->RemoteEndpoint());
+            conn->Close();
+        }
+        connections_.clear();
+    }
+
+    void Dispatch(ziapi::http::IRequestOutputQueue::ValueType &res)
+    {
+        std::scoped_lock sl(mu_);
+
+        // // If content length has not been calculated then set it. (some clients fail when content length is not set.)
+        if (res.first.headers.find(ziapi::http::header::kContentLength) == res.first.headers.end()) {
+            res.first.headers[ziapi::http::header::kContentLength] = std::to_string(res.first.body.size());
+        }
+        // If connection header is missing from the response then set it to close.
+        if (res.first.headers.find(ziapi::http::header::kConnection) == res.first.headers.end()) {
+            res.first.headers[ziapi::http::header::kConnection] = "close";
+        }
+
+        for (auto &conn : connections_) {
+            auto remote = conn->RemoteEndpoint();
+            if (remote.address().to_string() == std::any_cast<std::string>(res.second.at("client.socket.address")) &&
+                remote.port() == std::any_cast<std::uint16_t>(res.second.at("client.socket.port"))) {
+                if (std::any_cast<std::string>(res.second.at("http.connection")) != "close") {
+                    conn->ShouldClose(false);
+                }
+                conn->Send(res.first);
+            }
+        }
+    }
+
+private:
+    std::mutex mu_;
+
+    std::vector<SharedConnection> connections_;
+};
