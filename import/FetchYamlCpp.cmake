7a22e073227c6c824a652ac94480a2b231bce862
Martin Olivier <martin.olivier@live.fr>
2022-03-04 09:58:39 +0100

    fix: build system

--- import/FetchYamlCpp.cmake
+++ import/FetchYamlCpp.cmake
@@ -9,6 +9,7 @@ FetchContent_Declare(
 )
 
 set(YAML_CPP_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(YAML_CPP_BUILD_TOOLS OFF CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(yamlcpp)
 include_directories(${yamlcpp_SOURCE_DIR}/include)
 
@@ -17,4 +18,4 @@ if(NOT yamlcpp_POPULATED)
   add_subdirectory(yamlcpp)
 endif()
 
-link_directories(${yamlcpp_INSTALL_DIR}/src/libyaml-cpp-build)
+link_directories(${yamlcpp_BINARY_DIR})
